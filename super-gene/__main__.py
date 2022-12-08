9f0cb1bd7636459043420e4c5e5b39dadb6c1cf2
maxludden <dev@maxludden.com>
2022-12-08 10:48:42 -0500

    fixed poetry

---
+++ super-gene/__main__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+from
+
+from maxconsole import get_console, get_theme
+from maxprogress import get_progress
+from maxcolor import gradient, gradient_panel
+from maxsetup import new_run
